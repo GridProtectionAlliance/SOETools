0c08f7b2f16c91734d08098c467df6e80cbd9243
StephenCWills <swills@gridprotectionalliance.org>
2016-04-28 08:26:19 -0400

    Fixed CSS bundle for jquery-ui in BundleConfig.cs.

--- Source/WebSite/App_Start/BundleConfig.cs
+++ Source/WebSite/App_Start/BundleConfig.cs
@@ -94,7 +94,7 @@ namespace SOETools
                         "~/Content/fileinput.css"));
 
             bundles.Add(new StyleBundle("~/css.bundle/jqueryui").Include(
-                        "~/Content/themes/base/all.css"));
+                        $"~/Content/jquery-ui{min}.css"));
 
             bundles.Add(new StyleBundle("~/css.bundle/primeui").Include(
                         "~/Content/primeui-theme.css",
