2dd93834139107a590a2e4366bc6562fc2353d94
StephenCWills <swills@gridprotectionalliance.org>
2016-05-06 10:49:07 -0400

    Moved logic to convert to simple icons to a separate .js file on CycleDataSOEPointView.

---
+++ Source/WebSite/Scripts/CycleDataSOEPointView.js
@@ -0,0 +1,37 @@
+﻿function getSimpleVoltageState(state) {
+    var simpleState;
+
+    if (state.indexOf('0') >= 0)
+        simpleState = '3';
+    else if (state.indexOf('2') >= 0)
+        simpleState = '1';
+    else if (state.indexOf('3') >= 0)
+        simpleState = '2';
+    else
+        simpleState = '0';
+
+    return simpleState;
+}
+
+function getSimpleBreakerStatus(data) {
+    var summaryState;
+    var simpleState;
+
+    summaryState =
+        data.BreakerElementA +
+        data.BreakerElementC +
+        data.BreakerElementB;
+
+    if (summaryState.indexOf('5') >= 0)
+        simpleState = '5';
+    else if (data.StatusElement.startsWith('3'))
+        simpleState = '4'
+    else if (summaryState.indexOf('4') >= 0)
+        simpleState = '2';
+    else if (summaryState.indexOf('0') >= 0)
+        simpleState = '3'
+    else
+        simpleState = '1';
+
+    return simpleState;
+}
